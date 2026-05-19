c9fcab8ece55cde02e4c7c11e606dafe7667c7ff
stai1 <stai1@alumni.scu.edu>
2026-05-18 17:38:55 -0700

    install gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/chord-reading-trainer/',
   plugins: [react()],
   assetsInclude: ['**/*.opus'],
 })
